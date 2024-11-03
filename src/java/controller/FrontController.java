6c8bebe3e038e13a9679f67833e9fa6db1b04725
boral <boral.rong@student.tafesa.edu.au>
2024-11-03 13:29:41 +1030

    Created Dispatchers

--- src/java/controller/FrontController.java
+++ src/java/controller/FrontController.java
@@ -9,25 +9,28 @@ import model.Book;
 import model.CartItem;
 import utility.AdmitBookStoreDAO;
 
-public class FrontController extends HttpServlet {
+public class FrontController extends HttpServlet 
+{
 
     private final HashMap actions = new HashMap();
 
     //Initialize global variables
-    public void init(ServletConfig config) throws ServletException {
+    public void init(ServletConfig config) throws ServletException 
+    {
         super.init(config);
-
     }
 
     //Process the HTTP Get request
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
         System.err.println("doGet()");
         doPost(request, response);
 
     }
 
     //Process the HTTP Post request
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
         response.setContentType("text/html");
 
         String requestedAction = request.getParameter("action"); //"Action" changed to "action"
@@ -35,31 +38,41 @@ public class FrontController extends HttpServlet {
         AdmitBookStoreDAO dao = new AdmitBookStoreDAO();
         String nextPage = "";
 
-        if (requestedAction == null) {
+        if (requestedAction == null) 
+        {
             dao = new AdmitBookStoreDAO();
             List books = null;
             nextPage = "/jsp/error.jsp";
             session = request.getSession();
-            try {
+            try 
+            {
                 books = dao.getAllBooks();
                 session.setAttribute("books", books); //Books changed to books
                 nextPage = "/jsp/titles.jsp";
 
-            } catch (Exception ex) {
+            } 
+            catch (Exception ex) 
+            {
                 request.setAttribute("result", ex.getMessage());
                 nextPage = "/jsp/error.jsp";
-            } finally {
+            } 
+            finally 
+            {
                 this.dispatch(request, response, nextPage);
             }
-        } else if (requestedAction.equals("add_to_cart")) {
+        } 
+        else if (requestedAction.equals("add_to_cart")) 
+        {
             nextPage = "/jsp/titles.jsp";
 
             Map cart = (Map) session.getAttribute("cart");
             String[] selectedBooks = request.getParameterValues("add");
 
-            if (cart == null) {
+            if (cart == null) 
+            {
                 cart = new HashMap();
-                for (int i = 0; i < selectedBooks.length; i++) {
+                for (int i = 0; i < selectedBooks.length; i++) 
+                {
                     String isbn = selectedBooks[i];
                     int quantity = Integer.parseInt(request.getParameter(isbn));
                     Book book = this.getBookFromList(isbn, session);
@@ -68,14 +81,19 @@ public class FrontController extends HttpServlet {
                     cart.put(isbn, item);
                 }
                 session.setAttribute("cart", cart);
-            } else {
-                for (int i = 0; i < selectedBooks.length; i++) {
+            } 
+            else 
+            {
+                for (int i = 0; i < selectedBooks.length; i++) 
+                {
                     String isbn = selectedBooks[i];
                     int quantity = Integer.parseInt(request.getParameter(isbn));
                     if (cart.containsKey(isbn)) {
                         CartItem item = (CartItem) cart.get(isbn);
                         item.setQuantity(quantity);
-                    } else {
+                    }
+                    else 
+                    {
                         Book book = this.getBookFromList(isbn, session); //Changed Books to Book
                         CartItem item = new CartItem(book);
                         item.setQuantity(quantity);
@@ -83,7 +101,6 @@ public class FrontController extends HttpServlet {
                     }
                 }
             }
-
             this.dispatch(request, response, nextPage);
         } 
         else if (requestedAction.equals("checkout")) //Changed Checkout to checkout
@@ -94,10 +111,11 @@ public class FrontController extends HttpServlet {
         } 
         else if (requestedAction.equals("continue")) //Changed Continue to continue
         {
-
             nextPage = "/jsp/titles.jsp";
             this.dispatch(request, response, nextPage);
-        } else if (requestedAction.equals("update_cart")) {
+        } 
+        else if (requestedAction.equals("update_cart")) 
+        {
 
             Map cart = null;
             CartItem item = null;
@@ -105,14 +123,17 @@ public class FrontController extends HttpServlet {
             nextPage = "/jsp/cart.jsp";
             cart = (Map) session.getAttribute("cart");
             String[] booksToRemove = request.getParameterValues("remove");
-            if (booksToRemove != null) {
-                for (int i = 0; i < booksToRemove.length; i++) {
+            if (booksToRemove != null) 
+            {
+                for (int i = 0; i < booksToRemove.length; i++) 
+                {
                     cart.remove(booksToRemove[i]);
                 }
             }
             Set entries = cart.entrySet();
             Iterator iter = entries.iterator();
-            while (iter.hasNext()) {
+            while (iter.hasNext()) 
+            {
                 Map.Entry entry = (Map.Entry) iter.next();
                 isbn = (String) entry.getKey();
                 item = (CartItem) entry.getValue();
@@ -120,22 +141,28 @@ public class FrontController extends HttpServlet {
                 item.updateQuantity(quantity);
             }
             this.dispatch(request, response, nextPage);
-        } else if (requestedAction.equals("view_cart")) {
+        } 
+        else if (requestedAction.equals("view_cart")) 
+        {
             nextPage = "/jsp/cart.jsp";
             Map cart = (Map) session.getAttribute("cart");
-            if (cart == null) {
+            if (cart == null) 
+            {
                 nextPage = "/jsp/titles.jsp";
             }
             this.dispatch(request, response, nextPage);
         }
     }
 
-    private Book getBookFromList(String isbn, HttpSession session) {
+    private Book getBookFromList(String isbn, HttpSession session) 
+    {
         List list = (List) session.getAttribute("books");
         Book aBook = null;
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) 
+        {
             aBook = (Book) list.get(i);
-            if (isbn.equals(aBook.getIsbn())) {
+            if (isbn.equals(aBook.getIsbn())) 
+            {
                 break;
             }
         }
@@ -143,13 +170,15 @@ public class FrontController extends HttpServlet {
     }
 
     private void dispatch(HttpServletRequest request, HttpServletResponse response, String page) throws ServletException,
-            IOException {
+            IOException 
+    {
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(page);
         dispatcher.forward(request, response);
     }
 
     //Get Servlet information
-    public String getServletInfo() {
+    public String getServletInfo() 
+    {
         return "controller.FrontController Information";
     }
 
